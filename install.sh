3d916b0b0ea7c5f1f9bd72cf54389fdbcf6e14f3
TOGLinstar <andywang@princeton.edu>
2022-07-31 18:40:07 -0400

    install.sh yay fix

--- install.sh
+++ install.sh
@@ -74,8 +74,7 @@ fi
 
 if ! command -v yay &>/dev/null; then
   echo "Installing yay..."
-  sudo pacman -S --needed git base-devel && git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si
-  rm -rf yay
+  sudo pacman -S --needed git base-devel && git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si && cd .. && rm -rf yay
 fi
 
 echo "Installation and linking complete."
