ac1dcd2cccc34d28929eadfd5215c5287f4486ea
ProfessorLinstar <awaw6465@gmail.com>
2022-08-07 19:07:33 -0400

    dconf left/right split, install.sh

--- install.sh
+++ install.sh
@@ -9,6 +9,7 @@
 
 # Ensure that cwd is at ~/dotfiles
 DOTFILES_ROOT="$HOME/dotfiles"
+BACKUPS_ROOT="$DOTFILES_ROOT/.backup"
 if ! ([[ -d "$DOTFILES_ROOT" ]] && cd "$DOTFILES_ROOT"); then
   echo "dotfiles must be at $DOTFILES_ROOT."
   exit 1
@@ -28,8 +29,8 @@ function confirmsed {
     echo "Warning: $file does not exist." 
 
   elif grep -Eq "^$pattern$" < "$file" && confirm "Edit $file to replace '$pattern' with '$replace'?"; then
-    local backup="$DOTFILES_ROOT/.backup$file"
-    mkdir -pv "$(dirname $backup)" && cp -v "$file" "$backup"
+    local backup="$BACKUPS_ROOT$file"
+    mkdir -pv "$(dirname $backup)" && cp -nvi "$file" "$backup" < /dev/tty
     $user sed -Ei "s@^$pattern\$@$replace@" "$file"
 
   else
@@ -39,13 +40,13 @@ function confirmsed {
 
 
 TERMINAL_PACMAN=(
-  "zsh"                                                         #
-  "zsh-theme-powerlevel10k"                                     #
-  "zsh-autosuggestions"                                         #
-  "zsh-syntax-highlighting"                                     #
-  "zsh-history-substring-search"                                #
-  "zsh-completions"                                             #
-  "tmux"                                                        #
+  "zsh"                                                         # zshell essentials
+  "zsh-theme-powerlevel10k"                                     # .
+  "zsh-autosuggestions"                                         # .
+  "zsh-syntax-highlighting"                                     # .
+  "zsh-history-substring-search"                                # .
+  "zsh-completions"                                             # .
+  "tmux"                                                        # terminal multiplexer
   "git"                                                         # install.sh
   "make"                                                        # Lunarvim dependency
   "python"                                                      # Lunarvim dependency
@@ -53,69 +54,69 @@ TERMINAL_PACMAN=(
   "cargo"                                                       # Lunarvim dependency
   "ripgrep"                                                     # Lunarvim telescope dependency
   "fd"                                                          # Lunarvim telescope dependency
-  "neovim"                                                      # Text editor
-  "noto-fonts"                                                  #
-  "noto-fonts-cjk"                                              #
-  "noto-fonts-emoji"                                            #
-  "noto-fonts-extra"                                            #
-  "tree"                                                        #
-  "cmus"                                                        #
-  "locate"                                                      #
-  "networkmanager"                                              #
-  "xclip"                                                       #
-  "man"                                                         #
-  "man-pages"                                                   #
+  "neovim"                                                      # text editor
+  "noto-fonts"                                                  # special fonts
+  "noto-fonts-cjk"                                              # .
+  "noto-fonts-emoji"                                            # .
+  "noto-fonts-extra"                                            # .
+  "tree"                                                        # show directory contents in tree form
+  "cmus"                                                        # Music player
+  "locate"                                                      # locate files
+  "networkmanager"                                              # ensure internet is available
+  "xclip"                                                       # system clipboard tool
+  "man"                                                         # manual
+  "man-pages"                                                   # manual database
 )
 
 GNOME_PACMAN=(
-  "gnome-shell"                                                 #
-  "gdm"                                                         #
-  "gnome-terminal"                                              #
-  "gnome-tweaks"                                                #
-  "gnome-control-center"                                        #
-  "gparted"                                                     #
-  "dconf-editor"                                                #
-  "papirus-icon-theme"                                          #
-  "nautilus"                                                    #
-  "gnome-screenshot"                                            #
+  "gnome-shell"                                                 # gnome desktop environment
+  "gdm"                                                         # gnome display manager
+  "gnome-terminal"                                              # terminal for gnome
+  "gnome-tweaks"                                                # more settings
+  "gnome-control-center"                                        # settings
+  "gparted"                                                     # disk partition editor
+  "dconf-editor"                                                # gnome settings editor
+  "papirus-icon-theme"                                          # nice app icon theme
+  "nautilus"                                                    # gui file explorer
+  "gnome-screenshot"                                            # screenshot tool
   "okular"                                                      # PDF viewer
-  "gnome-system-monitor"                                        # System monitor
-  "fragments"                                                   # Torrent downloader
-  "gthumb"                                                      # Image viewer
-  "totem"                                                       # Video player
-  "gst-libav"                                                   # Required multimedia framework for totem
+  "gnome-system-monitor"                                        # system monitor
+  "fragments"                                                   # torrent downloader
+  "gthumb"                                                      # image viewer
+  "totem"                                                       # video player
+  "gst-libav"                                                   # required multimedia framework for totem
   "kid3"                                                        # audio metadata editor
-  "bluez-utils"                                                 # Bluetooth support
-  "discord"                                                     # Social media
-  "qgnomeplatform-qt5"                                          # Gnome themes (adwaita) for qt5 applications
+  "bluez-utils"                                                 # bluetooth support
+  "discord"                                                     # social media
+  "qgnomeplatform-qt5"                                          # gnome themes (adwaita) for qt5 applications
 )
 
 LATEX_PACMAN=(
-  "texlive-most"                                                # Provide most latex packages
-  "texlive-bibtexextra"                                         # Enable biblatex
-  "biber"                                                       # Enable biber for latexmk
-  "perl-clone"                                                  # Fix missing dependency for biber (08-05-2022)
+  "texlive-most"                                                # provide most latex packages
+  "texlive-bibtexextra"                                         # enable biblatex
+  "biber"                                                       # enable biber for latexmk
+  "perl-clone"                                                  # fix missing dependency for biber (08-05-2022)
   "cpanminus"                                                   # install cpan modules more easily
 )
 
 TERMINAL_YAY=(
-  "nerd-fonts-ubuntu-mono"                                      #
+  "nerd-fonts-ubuntu-mono"                                      # nerd font
 )
 
 GNOME_YAY=(
-  "adw-gtk-theme"                                               #
-  "xcursor-breeze"                                              #
-  "insync"                                                      #
+  "adw-gtk-theme"                                               # dark gtk theme
+  "xcursor-breeze"                                              # cursor theme
+  "insync"                                                      # drive sync
   "google-chrome"                                               #
 )
 
 # Exclude paths beginning with these prefixes when linking
 EXCLUDE_PATHS=(
-  "./.git"                                                      #
-  "./dump"                                                      #
-  "./install.sh"                                                #
-  "./README.md"                                                 #
-  "./.backup"                                                   #
+  "./.git"                                                      # git information
+  "./dump"                                                      # manually loaded configuration files
+  "./install.sh"                                                # this script!
+  "./README.md"                                                 # dotfiles readme
+  "./.backup"                                                   # temporary backup file of modified files
 )
 
 # Packages
@@ -186,7 +187,7 @@ while read dotfile; do
   fi
 
   [[ -d $(dirname $target) ]] || $user mkdir -pv $(dirname $target)
-  [[ -f $target ]] && [[ ! -L $target ]] && $user rm -vi $target
+  [[ -f $target ]] && [[ ! -L $target ]] && $user mv -vi $target "$BACKUPS_ROOT$target" < /dev/tty
   [[ -L $target ]] || $user ln -sv $(sed "s@^\.\(.*\)@"$(pwd)"\1@" <<< $dotfile) $target
 
 done <<< $(find . -type f -print | grep -Ev $(tr " " "|" <<< ${EXCLUDE_PATHS[@]}) )
