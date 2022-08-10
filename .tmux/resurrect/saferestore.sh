42923e6c132d468e5ada8c6983629811c9d622f8
ProfessorLinstar <awaw6465@gmail.com>
2022-08-10 14:01:40 -0400

    bash/sh compatability fix

--- .tmux/resurrect/saferestore.sh
+++ .tmux/resurrect/saferestore.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 usage() { echo "$0 safe|prev"; }
+cd $(dirname $0)
 
 # delete empty tmux_resurrect files
 find . -maxdepth 1 -size 0 -regex "^\./tmux_resurrect_.*\.txt$" -delete
@@ -18,12 +19,12 @@ case "$1" in
 esac
 
 # if last is empty, try to give it a resurrect file
-if [[ -z $last ]]; then
+if [ -z $last ]; then
   last=$(ls | grep -E "^tmux_resurrect_.*\.txt$" | tail -n 1)
 fi
 
 # if unable to find a resurrect file, then exit.
-if [[ -z $last ]]; then
+if [ -z $last ]; then
   echo "No tmux_resurrect_*.txt files available."
   exit 2
 else
@@ -32,7 +33,7 @@ fi
 
 # replace process with tmux-resurrect restore script
 restore="$HOME/.tmux/plugins/tmux-resurrect/scripts/restore.sh"
-if ! [[ -f $restore ]]; then
+if ! [ -f $restore ]; then
   echo "tmux-resurrect restore.sh script not found."
   exit 3
 else
