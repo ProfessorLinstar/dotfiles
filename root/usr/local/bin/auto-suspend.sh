7ce0626a3d20f5c89b4f498cd0d6e4e99acac6b0
ProfessorLinstar <awaw6465@gmail.com>
2022-10-24 17:46:47 -0400

    auto-suspend.sh complete

--- root/usr/local/bin/auto-suspend.sh
+++ root/usr/local/bin/auto-suspend.sh
@@ -3,6 +3,11 @@
 CRITICAL=10  # Critical battery percentage
 SERVICE=auto-suspend
 
+if ! command -pv acpi &>/dev/null; then
+  logger "$SERVICE: acpi not found. Unable to verify battery capacity."
+  exit 1
+fi
+
 while read -r battery status capacity; do
   if [ "$status" = Charging -o "$capacity" -gt "$CRITICAL" ]; then
     logger "$SERVICE: Battery $battery $status at $capacity% capacity."
