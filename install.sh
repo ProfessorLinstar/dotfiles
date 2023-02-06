f77bd50bf884642014d4b42ffdfe1812ef221a66
ProfessorLinstar <awaw6465@gmail.com>
2023-02-06 17:59:07 -0500

    lunarvim stable release install.sh

--- install.sh
+++ install.sh
@@ -289,7 +289,7 @@ if ! $SKIP_LUNARVIM; then
     echo "Installing Lunarvim..."
     sudo pacman --needed -S git make python npm cargo           # Lunarvim dependencies
     gio trash ~/.config/lvim/config.lua                         # Prevent installation from overwriting existing config
-    LV_BRANCH=rolling bash <(curl -s https://raw.githubusercontent.com/lunarvim/lunarvim/rolling/utils/installer/install.sh)
+    LV_BRANCH='release-1.2/neovim-0.8' bash <(curl -s https://raw.githubusercontent.com/lunarvim/lunarvim/fc6873809934917b470bff1b072171879899a36b/utils/installer/install.sh)
   fi
 fi
 
